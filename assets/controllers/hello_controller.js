81eaf42892316211e7fda963873f9e7d0a148093
elkira01 <elkira01@gmail.com>
2025-12-14 20:10:31 +0100

    refactor

---
+++ assets/controllers/hello_controller.js
@@ -0,0 +1,21 @@
+import { Controller } from '@hotwired/stimulus';
+import React from 'react';
+import {createRoot} from "react-dom/client";
+import Hello from "../react/src/views/HelloView";
+
+/*
+ * This is an example Stimulus controller!
+ *
+ * Any element with a data-controller="hello" attribute will cause
+ * this controller to be executed. The name "hello" comes from the filename:
+ * hello_controller.js -> "hello"
+ *
+ * Delete this file or adapt it for your use!
+ */
+export default class extends Controller {
+    static values = { fullName: String }
+    connect() {
+        const root = createRoot(this.element)
+        root.render(<Hello fullName={this.fullNameValue} />)
+    }
+}
