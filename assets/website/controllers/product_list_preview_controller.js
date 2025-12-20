d2afd4cdd368c15cdfb2b2c8490fb152b014e64e
elkira01 <elkira01@gmail.com>
2025-12-20 12:40:19 +0100

    feat(website): product preview

---
+++ assets/website/controllers/product_list_preview_controller.js
@@ -0,0 +1,15 @@
+import { Controller } from "@hotwired/stimulus";
+import { createRoot } from "react-dom/client";
+import { ProductPreviewComponent } from "../react/components/features/products/ProductPreviewComponent";
+import { products } from "../assets/data/products";
+
+export default class extends Controller {
+  static values = {
+    previewList: Array,
+  };
+
+  connect() {
+    const root = createRoot(this.element);
+    root.render(<ProductPreviewComponent previewList={products} />);
+  }
+}
