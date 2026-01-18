ab9ddad5c9f76e178310f82786e457318278b02e
elkira01 <elkira01@gmail.com>
2026-01-18 09:28:10 +0100

    feat(website): top header

--- assets/website/react/components/core/icons/BaseIcon.tsx
+++ assets/website/react/components/core/icons/BaseIcon.tsx
@@ -1,7 +1,7 @@
-import { ArrowLeft, ArrowRight, CalendarDays, MapPin, Phone } from "lucide-react";
+import {ArrowLeft, ArrowRight, CalendarCheck2, CalendarDays, MapPin, Phone} from "lucide-react";
 import React from "react";
 
-export type IconType = "arrow-right" | "arrow-left" | "calendar-days" | "phone" | "map-pin";
+export type IconType = "arrow-right" | "arrow-left" | "calendar-days" | "phone" | "map-pin"|"calendar-check";
 
 export function BaseIcon({ type, ...props }: { type: IconType; size?: number }) {
   const size = props.size ?? 24;
@@ -13,6 +13,8 @@ export function BaseIcon({ type, ...props }: { type: IconType; size?: number }) 
       return <ArrowLeft size={size} />;
     case "calendar-days":
       return <CalendarDays size={size} />;
+    case "calendar-check":
+      return <CalendarCheck2 size={size} />;
     case "phone":
       return <Phone size={size} />;
     case "map-pin":
